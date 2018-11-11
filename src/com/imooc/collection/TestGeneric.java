4b818cf38ce55c9a0090b3fbe12f08c4bd06a872
nico1988 <410827024@qq.com>
2018-11-12 07:56:56 +0800

    泛型1

---
+++ src/com/imooc/collection/TestGeneric.java
@@ -0,0 +1,38 @@
+package com.imooc.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestGeneric {
+	/**
+	 * 带有泛型 ---- Course 的list类型的属性
+	 * */
+	public List<Course> courses;
+	public TestGeneric() {
+		this.courses = new ArrayList<Course>();
+	}
+	/**
+	 * 测试添加
+	 * */
+	public void testAdd() {
+		Course cr1 = new Course("1","大学鱼粉");
+		courses.add(cr1);
+//		泛型集合中不能添加 规定的类型以外的对象，否则报错
+//		courses.add("2","dsfsdf");
+		Course cr2 = new Course("2","计算机基础");
+		courses.add(cr2);
+	}
+	public void testForEach() {
+		/**
+		 * 这里已经提前规定了类型
+		 * */
+		for(Course cr:courses) {
+			System.out.println(cr.id + ": " + cr.name);
+		}
+	}
+	public static void main(String[] args) {
+		TestGeneric tg = new TestGeneric();
+		tg.testAdd();
+		tg.testForEach();
+	}
+}
